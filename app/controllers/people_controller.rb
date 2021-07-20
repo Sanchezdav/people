67807973e9f739b1b91c5b50f9a65e2312ca550e
David Sanchez <david.sanchez@kueski.com>
2021-07-20 10:28:40 -0500

    Fetch people from the API
    It's necessary to build a Person model to manage the people we fetch
    from the API, this is the initial approach just to list people on the
    view.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,30 @@
 # frozen_string_literal
 
+require 'net/http'
+
 class PeopleController < ApplicationController
   before_action :verify_api_key
 
   def index
+    @people = people_list.map { |item| Person.new.from_json(item.to_json) }
   end
 
   private
 
   def verify_api_key
     redirect_to root_path, alert: 'API Key is not present' if ENV['API_KEY'].nil?
+  end
+
+  def people_list
+    api_key = ENV['API_KEY']
+    uri = URI('https://api.salesloft.com/v2/people.json')
+    req = Net::HTTP::Get.new(uri)
+    req['Authorization'] = "Bearer #{api_key}"
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
+      http.request(req)
+    }
+    parsed_response = JSON.parse(response.body)
+    parsed_response['data']
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,18 @@
 class Person
   include ActiveModel::Serializers::JSON
 
-  attr_accessor :id, :first_name, :last_name, :display_name, :email_address,
-                :title, :created_at, :updated_at
+  # For now I just want to build my Person object with these attributes
+  PERMITTED_ATTRIBUTES = ['id', 'display_name', 'email_address', 'title']
+
+  attr_accessor :id, :display_name, :email_address, :title
+
+  def attributes=(hash)
+    hash.each do |key, value|
+      send("#{key}=", value) if PERMITTED_ATTRIBUTES.include?(key)
+    end
+  end
+
+  def attributes
+    instance_values
+  end
 end
